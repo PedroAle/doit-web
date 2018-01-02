9179f6952a898acb06f52bb63365a2ea129af28b
annemarierolo <annemarierolo@gmail.com>
2018-01-02 15:12:19 -0500

    Cambio alertas/datepicker SignUp

--- js/logIn.js
+++ js/logIn.js
@@ -1,75 +1,35 @@
-      function SubForm () {
-          validarForm();
-          var username = $("#usuario").val();
-          var password = $("#password").val();
-          //Armamos el JSON que vamos a mandar al server
-          var data = { "username": username, "password": password};
-          //Con stringify nos aseguramos de que se vuelva un JSON valido (Aqui solo lo imprimimos pero en el ajax si lo cambiamos)
-          console.log(JSON.stringify(data));
-          $.ajax({
-            url:'https://api-doit.herokuapp.com/login',
-            type:'post',
-            //SUPER IMPORTANTE ESTOS DOS ATRIBUTOS (contentType y dataType)
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            data: JSON.stringify(data),
-            //Aqui manejamos el caso si todo va bien y el server responde con un HTTP 2XX
-            success:function(data){
-              //Este objeto data es la respuesta JSON del servidor, que devuelve el JSON del usuario que se acaba de loggear
-                console.log(data);
-                alert("Iniciaste sesión");
-                window.location.href = "notes.html";
-            },
-            error: function(data) {
-              alert("Nombre de usuario o contraseña invalido")}
-            });
+function SubForm () {
+  validarForm();
+  var username = $("#usuario").val();
+  var password = $("#password").val();
+  //Armamos el JSON que vamos a mandar al server
+  var data = { "username": username, "password": password};
+  //Con stringify nos aseguramos de que se vuelva un JSON valido (Aqui solo lo imprimimos pero en el ajax si lo cambiamos)
+  console.log(JSON.stringify(data));
+    $.ajax({
+      url:'https://api-doit.herokuapp.com/login',
+      type:'post',
+      //SUPER IMPORTANTE ESTOS DOS ATRIBUTOS (contentType y dataType)
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify(data),
+      //Aqui manejamos el caso si todo va bien y el server responde con un HTTP 2XX
+      success:function(data){
+        //Este objeto data es la respuesta JSON del servidor, que devuelve el JSON del usuario que se acaba de loggear
+        console.log(data);
+        alert("Iniciaste sesión");
+        window.location.href = "notes.html";
+      },
+      error: function(data) {
+        if (data.status == 400) {
+          alert("Usuario o Contraseña invalida");
+        }
       }
+    });
+}
 
+function validarForm(){
 
+  //arreglar
 
-      function validateNombreU() {
-          var usuario = $('#usuario').val();
-          var checkUsuario = /([A-Za-z]+[0-9])|([A-Za-z])/;
-          if(!usuario.match(checkUsuario)) {
-              alert("Campo Nombre de usuario invalido");
-              $('#usuario').val("");
-              $('#usuario').focus();
-              return false;
-          }else{
-              return true;
-          }
-      }
-
-      function validatePassword() {
-          var password = $('#password').val();
-          var checkPassword = /(([\d]+[A-Za-z]+)|[A-Za-z]+[\d]+$)/;
-          if(!password.match(checkPassword)) {
-              alert("Ingresar caracteres alfanumerico a la contraseña");
-              $('#password').val("");
-              $('#password').focus();
-              return false;
-          }else{
-              return true;
-          }
-      }
-
-      function validarForm(){
-          var flag = true;
-
-          if($('#usuario').val()==""){
-              alert("Debe ingresar nombre de usuario");
-              //$("#nombre").focus();
-              flag=false;
-          }else if($("#usuario").val()!=""){
-              flag = validateNombreU();
-          }
-
-          if($('#password').val()==""){
-              alert("Debe ingresar la contraseña");
-              //$("#apellido").focus();
-              flag=false;
-          } else if($("#password").val()!=""){
-              flag = validatePassword();
-          }
-          return flag;
-      }
+}
--- js/signUp.js
+++ js/signUp.js
@@ -1,233 +1,313 @@
 
-			function SubForm () {
-					validarForm();
-					var nombre = $("#nombre").val();
-					var apellido = $("#apellido").val();
-					var username = $("#usuario").val();
-					var dia = $("#day").val();
-					var mes = $("#month").val();
-					var año = $("#year").val();
-					var fechaDeNacimiento = $("#year").val()+"-"+$("#month").val()+"-"+$("#day").val()+"T00:00:00.000Z";
-					var password = $("#password").val();
-					var formaDeRegistro = 1;
-					//Armamos el JSON que vamos a mandar al server
-					var data = { "username": username, "nombre": nombre, "apellido": apellido, "fechaDeNacimiento": fechaDeNacimiento, "password": password, "formaDeRegistro": formaDeRegistro};
-					//Con stringify nos aseguramos de que se vuelva un JSON valido (Aqui solo lo imprimimos pero en el ajax si lo cambiamos)
-					console.log(JSON.stringify(data));
-					$.ajax({
-						url:'https://api-doit.herokuapp.com/users/register',
-						type:'POST',
-						//SUPER IMPORTANTE ESTOS DOS ATRIBUTOS (contentType y dataType)
-						contentType: "application/json; charset=utf-8",
-						dataType: "json",
-						data: JSON.stringify(data),
-						//Aqui manejamos el caso si todo va bien y el server responde con un HTTP 2XX
-						success:function(data){
-							//Este objeto data es la respuesta JSON del servidor, que devuelve el JSON del usuario que se acaba de loggear
-								console.log(data);
-								alert("Usuario registrado");
-								window.location.href = "home.html";
-						},
-            error: function(data) {
-              alert("Algun campo invalido")}
-            });
+function SubForm () {
+	validarForm();
+	var nombre = $("#nombre").val();
+	var apellido = $("#apellido").val();
+	var username = $("#usuario").val();
+	var date = $("#date").val();
+	var arr = date.split("/");
+	var fechaDeNacimiento = (arr[2]+"-"+arr[1]+"-"+arr[0]+"T00:00:00.000Z");
+	var password = $("#password").val();
+	var formaDeRegistro = 1;
+	//Armamos el JSON que vamos a mandar al server
+	var data = { "username": username, "nombre": nombre, "apellido": apellido, "fechaDeNacimiento": fechaDeNacimiento, "password": password, "formaDeRegistro": formaDeRegistro};
+	//Con stringify nos aseguramos de que se vuelva un JSON valido (Aqui solo lo imprimimos pero en el ajax si lo cambiamos)
+	console.log(JSON.stringify(data));
+	$.ajax({
+		url:'https://api-doit.herokuapp.com/users/register',
+		type:'POST',
+		//SUPER IMPORTANTE ESTOS DOS ATRIBUTOS (contentType y dataType)
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		data: JSON.stringify(data),
+		//Aqui manejamos el caso si todo va bien y el server responde con un HTTP 2XX
+		success:function(data){
+			//Este objeto data es la respuesta JSON del servidor, que devuelve el JSON del usuario que se acaba de loggear
+			console.log(data);
+			alert("Usuario registrado");
+			window.location.href = "home.html";
+		},
+        error: function(data) {
+			if (data.status == 400) {
+	          alert("No se pudo registrar el usuario");
+	        }
+		}
+    });
+}
+
+function validarForm(){
+
+	var nombre = $("#nombre").val();
+	var noNumerosNm = /[0-9]/;
+	var checknombre =/^[a-zA-Z_áéíóúñ\s]*$/;
+	$(document).trigger("clear-alert-id.nombre");
+	if (nombre.length < 1) {
+		$(document).trigger("set-alert-id-nombre", [
+			{
+				message: "Debe ingresar un nombre",
+				priority: "error"
 			}
+		]);
+		$('#nombre').val("");
+		}else if ((nombre.length < 3) && (nombre.match(checknombre))) {
+			$(document).trigger("set-alert-id-nombre", [
+				{
+					message: "Debe ingresar al menos 3 caracteres",
+					priority: "error"
+				}
+			]);
+			$('#nombre').val("");
+			}else if ((nombre.length < 3) && (!nombre.match(noNumerosNm))) {
+				$(document).trigger("set-alert-id-nombre", [
+					{
+						message: "Debe ingresar al menos 3 caracteres. No se aceptan caracteres especiales",
+						priority: "error"
+					}
+				]);
+				$('#nombre').val("");
+			}else if ((nombre.length < 3) && ((!nombre.match(checknombre)) || (!nombre.match(noNumerosNm)))) {
+				$(document).trigger("set-alert-id-nombre", [
+					{
+						message: "Debe ingresar al menos 3 caracteres. No se aceptan numeros",
+						priority: "error"
+					}
+				]);
+				$('#nombre').val("");
+			}else{
+				if(!nombre.match(checknombre)) {
+					$(document).trigger("set-alert-id-nombre", [
+						{
+							message: "Solo se aceptan letras",
+							priority: "error"
+						}
+					]);
+					$('#nombre').val("");
+				}
+			}
+<!---------------------------------------------------------------------------------------------------------------->
 
+	var apellido = $("#apellido").val();
+	var noNumerosAp = /[0-9]/;
+	var checkApellido =/^[a-zA-Z_áéíóúñ\s]*$/;
+	$(document).trigger("clear-alert-id.apellido");
+	    if (apellido.length < 1) {
+		    $(document).trigger("set-alert-id-apellido", [
+    		   {
+		          message: "Debe ingresar un apellido",
+		          priority: "error"
+		        }
+		    ]);
+			$('#apellido').val("");
+			}else if ((apellido.length < 3) && (apellido.match(checkApellido))) {
+		      	$(document).trigger("set-alert-id-apellido", [
+		        	{
+		          		message: "Debe ingresar al menos 3 caracteres",
+		          		priority: "error"
+		        	}
+		      	]);
+				$('#apellido').val("");
+		    }else if ((apellido.length < 3) && (!apellido.match(noNumerosAp))) {
+		      	$(document).trigger("set-alert-id-apellido", [
+		        	{
+		          		message: "Debe ingresar al menos 3 caracteres. No se aceptan caracteres especiales",
+		          		priority: "error"
+		        		}
+		      	]);
+				$('#apellido').val("");
+			}else if ((apellido.length < 3) && ((!apellido.match(checkApellido)) || (!apellido.match(noNumerosAp)))) {
+		    	$(document).trigger("set-alert-id-apellido", [
+		      		{
+		       			message: "Debe ingresar al menos 3 caracteres. No se aceptan numeros",
+		       			priority: "error"
+		       		}
+		    	]);
+					$('#apellido').val("");
+			}else{
+		    	if(!apellido.match(checkApellido)) {
+					$(document).trigger("set-alert-id-apellido", [
+			        	{
+			          		message: "Solo se aceptan letras",
+			          		priority: "error"
+			        	}
+			      	]);
+					$('#apellido').val("");
+				}
+		    }
 
-    function validateNombre() {
-        var nombre = $('#nombre').val();
-        var checkNombre = /^[a-zA-Z_áéíóúñ\s]*$/;
-        if(!nombre.match(checkNombre)) {
-            alert("Campo Nombre invalido");
-            $('#nombre').val("");
-            $('#nombre').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+<!---------------------------------------------------------------------------------------------------------------->
 
-    function validateApellido() {
-        var apellido = $('#apellido').val();
-        var checkApellido =/^[a-zA-Z_áéíóúñ\s]*$/;
-        if(!apellido.match(checkApellido)) {
-            alert("Campo Apellido invalido");
-            $('#apellido').val("");
-            $('#apellido').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+	var username = $("#usuario").val();
+	var checkUsuario = /([A-Za-z]+[0-9])|([A-Za-z])/;
+	var noEspecialesUs = /([A-Za-z]|([0-9]))/
+	$(document).trigger("clear-alert-id.usuario");
+		if (username.length < 1) {
+			$(document).trigger("set-alert-id-usuario", [
+				{
+					message: "Debe ingresar un usuario",
+					priority: "error"
+				}
+			]);
+			$('#usuario').val("");
+		}else if ((username.length < 6) && (!username.match(noEspecialesUs))) {
+			$(document).trigger("set-alert-id-usuario", [
+				{
+					message: "Debe ingresar al menos 6 caracteres. No se aceptan caracteres especiales",
+					priority: "error"
+				}
+			]);
+			$('#usuario').val("");
+		}else if (username.length < 6) {
+			$(document).trigger("set-alert-id-usuario", [
+				{
+					message: "Debe ingresar al menos 6 caracteres",
+					priority: "error"
+				}
+			]);
+		}else{
+			if(!username.match(checkUsuario)) {
+				$(document).trigger("set-alert-id-usuario", [
+				    {
+					    message: "Solo se aceptan letras o letras y numeros",
+					    priority: "error"
+				  	}
+			    ]);
+				$('#usuario').val("");
+			}
+		}
 
-    function validateNombreU() {
-        var usuario = $('#usuario').val();
-        var checkUsuario = /([A-Za-z]+[0-9])|([A-Za-z])/;
-        if(!usuario.match(checkUsuario)) {
-            alert("Campo Nombre de usuario invalido");
-            $('#usuario').val("");
-            $('#usuario').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+<!---------------------------------------------------------------------------------------------------------------->
 
-    function validatePassword() {
-        var password = $('#password').val();
-        var checkPassword = /(([\d]+[A-Za-z]+)|[A-Za-z]+[\d]+$)/;;
-        if(!password.match(checkPassword)) {
-            alert("Ingresar caracteres alfanumerico a la contraseña");
-            $('#password').val("");
-            $('#password').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+	var date = $("#date").val();
+	$(document).trigger("clear-alert-id.fecha");
+		if (date.length < 10) {
+			$(document).trigger("set-alert-id-fecha", [
+				{
+					message: "Debe ingresar una fecha",
+					priority: "error"
+				}
+			]);
+		}else {
+			var date = $("#date").val();
+			var arr = date.split("/");
+			if (arr[2]>1998){
+				$(document).trigger("set-alert-id-fecha", [
+				    {
+						message: "A;o incorrecto",
+				      	priority: "error"
+				    }
+				]);
+			}
+			if(arr[1]>12){
+				$(document).trigger("set-alert-id-fecha", [
+				    {
+				      	message: "Mes incorrecto",
+				      	priority: "error"
+				    }
+				]);
+			}
+			if(arr[0]>31){
+				$(document).trigger("set-alert-id-fecha", [
+				    {
+		     		    message: "Dia incorrecto",
+				      	priority: "error"
+				    }
+				]);
+			}
+			$('#date').val("");
+		}
 
-    function validatecpassword() {
-        var cpassword = $('#cpassword').val();
-        var checkcpassword = /(([\d]+[A-Za-z]+)|[A-Za-z]+[\d]+$)/;
-        //(([\d]+[A-Za-z]+)|[A-Za-z]+[\d]+$)/;
-        if(!cpassword.match(checkcpassword)) {
-            alert("Ingresar caracteres alfanumerico a la confirmación de contraseña");
-            $('#cpassword').val("");
-            $('#cpassword').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+<!---------------------------------------------------------------------------------------------------------------->
 
-    function validateDia() {
-        var dia = $('#day').val();
-        var checkdia = /([0-9])/;
-        if(!dia.match(checkdia)) {
-            alert("dia invalido");
-            $('#day').val("");
-            $('#day').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+	var password = $('#password').val();
+	var checkPassword = /([A-Za-z]+[0-9])|([A-Za-z])|([0-9])/;
+	$(document).trigger("clear-alert-id.password");
+		if (password.length < 1) {
+			$(document).trigger("set-alert-id-password", [
+				{
+					message: "Debe ingresar una contrase;a",
+					priority: "error"
+				}
+			]);
+			$('#password').val("");
+		}else if ((password.length < 8) && (password.match(checkPassword))) {
+			$(document).trigger("set-alert-id-password", [
+				{
+					message: "Debe ingresar al menos 8 caracteres",
+					priority: "error"
+				}
+			]);
+			$('#password').val("");
+		}else if ((password.length < 8) && (!password.match(checkPassword))) {
+			$(document).trigger("set-alert-id-password", [
+				{
+					message: "Debe ingresar al menos 8 caracteres. No se aceptan caracteres especiales",
+					priority: "error"
+				}
+			]);
+			$('#password').val("");
+		}else{
+			var password = $('#password').val();
+		  if(!password.match(checkPassword)) {
+				$(document).trigger("set-alert-id-password", [
+					{
+						message: "Solo se aceptan letras o numeros",
+						priority: "error"
+					}
+				]);
+				$('#password').val("");
+			}
+		}
 
-    function validateMes() {
-        var mes = $('#month').val();
-        var checkmes = /([0-9])/;
-        if(!mes.match(checkmes)) {
-            alert("mes invalido");
-            $('#month').val("");
-            $('#month').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+<!---------------------------------------------------------------------------------------------------------------->
 
-    function validateaño() {
-        var year = $('#year').val();
-        var checkaño = /([0-9])/;
-        if(!year.match(checkaño)) {
-            alert("año invalido");
-            $('#year').val("");
-            $('#year').focus();
-            return false;
-        }else{
-            return true;
-        }
-    }
+	var cpassword = $('#cpassword').val();
+	var checkcPassword = /([A-Za-z]+[0-9])|([A-Za-z])|([0-9])/;
+	$(document).trigger("clear-alert-id.cpassword");
+		if (password.length < 1) {
+			$(document).trigger("set-alert-id-cpassword", [
+				{
+					message: "Debe ingresar una contrase;a",
+					priority: "error"
+				}
+			]);
+			$('#cpassword').val("");
+		}else if ((password.length < 8) && (password.match(checkcPassword))){
+			$(document).trigger("set-alert-id-cpassword", [
+				{
+					message: "Debe ingresar al menos 8 caracteres",
+					priority: "error"
+				}
+			]);
+			$('#cpassword').val("");
+		}else if ((password.length < 8) && (!password.match(checkcPassword))){
+			$(document).trigger("set-alert-id-cpassword", [
+				{
+					message: "Debe ingresar al menos 8 caracteres. No se aceptan caracteres especiales",
+					priority: "error"
+				}
+			]);
+			$('#cpassword').val("");
+		}else{
+			var cpassword = $('#cpassword').val();
+		    if(!cpassword.match(checkcPassword)) {
+				$(document).trigger("set-alert-id-cpassword", [
+					{
+						message: "Solo se aceptan letras o numeros",
+						priority: "error"
+					}
+				]);
+				$('#cpassword').val("");
+			}
+		}
 
-    function validarForm(){
-        var flag = true;
-
-        if($('#nombre').val()==""){
-            alert("Debe ingresar nombre");
-            flag=false;
-        }else if($("#nombre").val()!=""){
-            flag = validateNombre();
-        }
-
-        if($('#apellido').val()==""){
-            alert("Debe ingresar apellido");
-            flag=false;
-        } else if($("#apellido").val()!=""){
-            flag = validateApellido();
-        }
-
-        if($('#usuario').val()==""){
-            alert("Debe ingresar nombre de usuario");
-          flag=false;
-        } else if($("#usuario").val()!=""){
-            flag = validateNombreU();
-        }
-
-        if($('#day').val()==""){
-            alert("Debe ingresar dia");
-            flag=false;
-        } else if($("#day").val()!=""){
-            flag = validateDia();
-        }
-
-        if($('#month').val()==""){
-            alert("Debe ingresar mes");
-            flag=false;
-        } else if($("#month").val()!=""){
-            flag = validateMes();
-        }
-
-        if($('#year').val()==""){
-            alert("Debe ingresar año");
-            flag=false;
-        } else if($("#year").val()!=""){
-            flag = validateaño();
-        }
-
-        if($('#password').val()==""){
-            alert("Debe ingresar contraseña");
-            flag=false;
-        } else if($("#password").val()!=""){
-            flag = validatePassword();
-        }
-        if($('#cpassword').val()==""){
-            alert("Debe confirmar la contraseña");
-              flag=false;
-        } else if($("#cpassword").val()!=""){
-            flag = validatecpassword();
-          }
-
-          if ($("#day").val()>"31"){
-            alert("Dia invalido");
-            $('#day').val("");
-            $('#day').focus();
-            return false;
-          }
-
-          if ($("#month").val()>"12"){
-            alert("Mes invalido");
-            $('#month').val("");
-            $('#month').focus();
-            return false;
-          }
-
-          if (($("#year").val()>"1999")||($("#year").val()<"1930")){
-            alert("Año invalido");
-            $('#year').val("");
-            $('#year').focus();
-            return false;
-          }
-
-        if ($("#password").val()!=$("#cpassword").val()){
-          alert("Deben coincidir las contraseñas");
-          $('#password').val("");
-          $('#password').focus();
-          $('#cpassword').val("");
-          $('#cpassword').focus();
-          return false;
-        }else {
-          return true;
-        }
-        return flag;
-
-    }
+		if ($("#password").val()!=$("#cpassword").val()){
+			$(document).trigger("set-alert-id-cpassword", [
+				{
+					message: "No coinciden ambas contrase;as",
+					priority: "error"
+				}
+			]);
+	        $('#password').val("");
+	        $('#cpassword').val("");
+	    }
+}
