24542eaa741770052b8c9d78f236ab9a0b512007
pedroale <pedro.alejandro.pt@gmail.com>
2018-01-04 15:48:10 -0400

    Categorias Al Crear la Tarea

--- js/todo.js
+++ js/todo.js
@@ -6,14 +6,25 @@ app.controller('TodoController', function ($scope) {
   $scope.input1 = '';
   $scope.date = '';
   $scope.editing = null;
+  $scope.Category = [{
+                Id: 'Regular',
+                Name: 'Regular'
+            }, {
+                Id: 'Importante',
+                Name: 'Importante'
+            }, {
+                Id: 'Urgente',
+                Name: 'Urgente'
+            }];
+   
   $scope.todos = [
-    {title: 'Titulo de la tarea' , description: 'Descripción de la tarea... PRUEBA AHORA!!!', date: "Fecha de la tarea"},
+    {title: 'Titulo de la tarea' , description: 'Descripción de la tarea... PRUEBA AHORA!!!', date: "Fecha de la tarea", category: 'Urgente'},
   ];
 
   $scope.add = add;
   $scope.remove = remove;
 
-  function add(input,input1,date) {
+  function add(input,input1,date,Category) {
     $(document).trigger("clear-alert-id.titulo");
     if (input.length < 1){
       $(document).trigger("set-alert-id-titulo", [
@@ -24,10 +35,23 @@ app.controller('TodoController', function ($scope) {
           ]);
       return
     } 
-    $scope.todos.push({title: input , description: input1 , date: date});
-    $scope.input = "";
-    $scope.input1 = "";
-    $scope.date = "";
+    if(!Category){
+      Category = "Regular";
+    }
+    $scope.todos.push({title: input , description: input1 , date: date , category: Category} );
+    $scope.input = '';
+    $scope.input1 = '';
+    $scope.input2 = null;
+    $scope.Category = [{
+                Id: 'Regular',
+                Name: 'Regular'
+            }, {
+                Id: 'Importante',
+                Name: 'Importante'
+            }, {
+                Id: 'Urgente',
+                Name: 'Urgente'
+            }];
   }
 
   function remove(todo) {
